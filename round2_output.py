7a94cccbf8021e20747ed2e111c30d2b45172699
Bugude Yogesh <23ata05052@gpcet.ac.in>
2025-07-26 22:03:28 +0530

    Add files via upload

---
+++ round2_output.py
@@ -0,0 +1,14 @@
+[
+    {
+        "question": "What methods are used?",
+        "answer": "deep learning algorithms"
+    },
+    {
+        "question": "What datasets are mentioned?",
+        "answer": "known  spam  and  non-spam  emails"
+    },
+    {
+        "question": "What future work is suggested?",
+        "answer": "Prescriptive Analytics"
+    }
+]
